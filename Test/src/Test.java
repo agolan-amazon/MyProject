3243eb2d8190f2a8ebe791a0648a8043180e9295
Karin Golan <Karingolan25@gmail.com>
2017-07-08 20:55:34 +0200

    Dev commit

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		//i love something 
-		System.out.println ("hhhhhello git"); 
+		System.out.println ("hhhhfaafffafhello git"); 
 		
 	}	
 
