4d9c9ed7a924abb3bf10bdf3160ce73076e6b4e5
Karin Golan <Karingolan25@gmail.com>
2017-07-08 21:13:28 +0200

    cooo

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		//i love something 
-		System.out.println ("ello git"); 
+		System.out.println ("hhhhfaafffafhello git"); 
 		
 	}	
 
