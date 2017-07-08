e17e82fda2b80fb8f0791f119540196d938d46ef
Karin Golan <Karingolan25@gmail.com>
2017-07-08 21:14:31 +0200

    The dev commited

--- Test/src/Test.java
+++ Test/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		//i love something 
-		System.out.println ("hhhhfaafffafhello git"); 
+		System.out.println ("Shalom"); 
 		
 	}	
 
